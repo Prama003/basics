e9cbda20b69f97dabb22d25d7f16373570020fbb
Prama003 <151642281+Prama003@users.noreply.github.com>
2024-12-17 16:35:25 +0000

    function

---
+++ .devcontainer/c++/basic_func.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+#include<cmath>
+using namespace std;
+void sayhi(string name, int age)
+{
+    cout<<"hello"<<name<<"you are"<< age<<endl;
+}
+int main()
+{
+    sayhi("mike",30);
+    sayhi("tom",45);
+    sayhi("john",67);
+    return 0;
+}
