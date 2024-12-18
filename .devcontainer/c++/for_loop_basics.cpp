6d927728ac6101882b4d5333060b63e01fa12f54
Prama003 <151642281+Prama003@users.noreply.github.com>
2024-12-18 16:04:52 +0000

    for loop basic

---
+++ .devcontainer/c++/for_loop_basics.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    for(int i=1;i<=5;i++) //enter the starting value, ending value and then the increement
+    {
+        cout<<i<<endl;
+    }
+    return 0;
+}
