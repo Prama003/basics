a538bbd3b6137f1d4877df514ccb2bb63e887d6b
Prama003 <151642281+Prama003@users.noreply.github.com>
2024-12-18 08:10:11 +0000

    guess game

---
+++ .devcontainer/c++/guess_game.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int secretnum=7;
+    int guessnum;
+    while(secretnum!=guessnum)
+    {
+        cout<<"enter the guess number";
+        cin>>guessnum;
+    }
+    cout<<"you win!";
+ return 0;
+}
