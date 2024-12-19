f3612c044be21acf2d997ffc987bc0e78080135b
Prama003 <151642281+Prama003@users.noreply.github.com>
2024-12-19 14:51:44 +0000

    pointers

---
+++ .devcontainer/c++/pointer.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int age= 19;
+    string name="tom";
+    double gpa=8.19;
+    cout<<"age"<<&age<<endl;
+    cout<<"name"<<&name<<endl;
+    cout<<"gpa"<<&gpa<<endl;
+    return 0;
+}
