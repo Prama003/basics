695862c81aa912db555dcd67ebe4540498164005
Prama003 <151642281+Prama003@users.noreply.github.com>
2024-12-19 14:43:24 +0000

    2d array

---
+++ .devcontainer/c++/2d_array.cpp
@@ -0,0 +1,8 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int num[3][2]={{2,3},{4,5},{6,7}}; 
+    // 3 denotes no of rows and 2 denotes no of columns if we consider in matrix form
+    cout<<num[2][1];
+}
